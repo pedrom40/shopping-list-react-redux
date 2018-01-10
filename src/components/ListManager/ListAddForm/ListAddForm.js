92586394d2cdc1bcf33c0e8e5b44ec1ce1e8470b
Pedro Morin <pedro.morin@usaa.com>
2018-01-10 08:42:11 -0600

    changed props to this.props

--- src/components/ListManager/ListAddForm/ListAddForm.js
+++ src/components/ListManager/ListAddForm/ListAddForm.js
@@ -15,28 +15,27 @@ export class ListAddForm extends React.Component {
         this.props.dispatch(addListItem(value));
         this.input.value = '';
         this.input.focus();
-
     }
 
     render() {
         return (
             <section>
                 <form onSubmit={e => this.onSubmit(e)}>
-                    <label htmlFor="addItem">{props.itemDefinitions.itemLabel}</label>
+                    <label htmlFor="addItem">{this.props.itemDefinitions.itemLabel}</label>
                     <input
                         type="text"
                         name="addItem"
                         id="addItem"
-                        placeholder={props.itemDefinitions.itemInputPlaceholder}
-                        style={props.formStyles.inputText}
+                        placeholder={this.props.itemDefinitions.itemInputPlaceholder}
+                        style={this.props.formStyles.inputText}
                         ref={input => this.textInput = input}
                     />
                     <input
                         type="submit"
                         name="submit"
                         id="submit"
-                        value={props.itemDefinitions.itemSubmitLabel}
-                        style={props.formStyles.inputSubmit}
+                        value={this.props.itemDefinitions.itemSubmitLabel}
+                        style={this.props.formStyles.inputSubmit}
                     />
                 </form>
             </section>
